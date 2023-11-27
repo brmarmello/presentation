00647b1517a62bb6bb7949de1c770dd6fee98d2d
Bruno Marmello <brmarmello@gmail.com>
2023-11-27 18:11:47 -0300

    Update streamlit_app.py
    Update presentation link

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation App')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQCqeDL5PnYToA6hPJ5YlcT1MiLyqhSqk4S0xqnMmDZ44KYhJ_nYGOZl1gRcz1bhncgN1OilHgi7AjJ/embed?start=false&loop=false&delayms=5000", height=432)
+components.iframe("https://docs.google.com/presentation/d/1Fs5r5OjusK7mrmOUVZ95V0vEPkAGISOU95IrYlJIlz8/edit?usp=sharing", height=432)
